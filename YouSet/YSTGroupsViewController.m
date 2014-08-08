99d3feebaaca700a50121bcaf423156d81952f13
willjuniorpp <willjrpp@gmail.com>
2014-08-08 14:26:45 -0300

    Add me and groups view controller

---
+++ YouSet/YSTGroupsViewController.m
@@ -0,0 +1,38 @@
+//
+//  YSTGroupsViewController.m
+//  YouSet
+//
+//  Created by Willian Pinho on 8/8/14.
+//  Copyright (c) 2014 YouSet. All rights reserved.
+//
+
+#import "YSTGroupsViewController.h"
+
+@interface YSTGroupsViewController ()
+
+@end
+
+@implementation YSTGroupsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
