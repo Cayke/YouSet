07875bfff67e97a907f1643bbdb6df46709a2702
willjuniorpp <willjrpp@gmail.com>
2014-08-08 15:35:14 -0300

    Classes

---
+++ YouSet/YSTCreateGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  YSTCreateGroupViewController.h
+//  YouSet
+//
+//  Created by Willian Pinho on 8/8/14.
+//  Copyright (c) 2014 YouSet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSTCreateGroupViewController : UIViewController
+
+@end
---
+++ YouSet/YSTProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  YSTProfileViewController.m
+//  YouSet
+//
+//  Created by Willian Pinho on 8/8/14.
+//  Copyright (c) 2014 YouSet. All rights reserved.
+//
+
+#import "YSTProfileViewController.h"
+
+@interface YSTProfileViewController ()
+
+@end
+
+@implementation YSTProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
