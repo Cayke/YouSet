68065e3ad041f1f565bd39491f7827011ecc2726
willjuniorpp <willjrpp@gmail.com>
2014-08-14 17:52:14 -0300

    Iniciol da implementação CreateNewToDo

---
+++ YouSet/YSTEditableTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  YSTEditableTableViewCell.h
+//  YouSet
+//
+//  Created by Willian Pinho on 8/14/14.
+//  Copyright (c) 2014 YouSet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSTEditableTableViewCell : UITableViewCell
+
+@end
