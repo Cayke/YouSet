45e113c9acfdce2d7020669988c5783dca7bf008
willjuniorpp <willjrpp@gmail.com>
2014-08-20 18:00:06 -0300

    Commit

---
+++ YouSet/YSTPropertiesOfCell.m
@@ -0,0 +1,14 @@
+//
+//  YSTPropertiesOfCell.m
+//  YouSet
+//
+//  Created by Willian Pinho on 8/20/14.
+//  Copyright (c) 2014 YouSet. All rights reserved.
+//
+
+#import "YSTPropertiesOfCell.h"
+
+@implementation YSTPropertiesOfCell
+
+
+@end
