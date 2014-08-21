046e2f46b741ad49a5309cc48f90eed521745575
riheldo <riheldomelo@hotmail.com>
2014-08-21 16:16:22 -0300

    developing connection

--- YouSet/YSTConnection.h
+++ YouSet/YSTConnection.h
@@ -15,24 +15,24 @@
 +(id)sharedConnection;
 
 // update todo, envia o todo para o server modificando-o ou criando
--(void)updateTodo:(YSTToDo*)todo;
+-(void)updateTodo:(YSTToDo*)todo; // assyncrono
 
 // login do usuario, cria o usuario, ou retorna os todos do usuario caso o usuario exista
--(NSDictionary*)login:(YSTUser*)user;
+-(NSDictionary*)login:(YSTUser*)user withError:(NSError*)error;
 
 // vai fazer uma requisicao dos todos do usuario passado
--(NSArray*)getTodosFromUser:(YSTUser*)user;
+-(NSArray*)getTodosFromUser:(YSTUser*)user withError:(NSError*)error;
 
 // o usuario do yst vai seguir ou deixar de seguir certo usuario
--(void)userDevice:(YSTUser*)mainUser willFollow:(BOOL)follow user:(YSTUser*)user;
+-(void)userDevice:(YSTUser*)mainUser willFollow:(BOOL)follow user:(YSTUser*)user; // assyncrono
 
 // pegar os seguidores de um user dado
--(NSArray*)getFollowersFromUser:(YSTUser*)user;
+-(NSArray*)getFollowersFromUser:(YSTUser*)user withError:(NSError*)error;
 
 // pegar seguidores do usuario do dispositivo
--(NSArray*)getFollowersFromDeviseUser;
+-(NSArray*)getFollowersFromDeviseUserWithError:(NSError*)error;
 
 // envia a lista de contatos do usuario do dispositivo e retorna os usuarios que sao do yst
--(NSArray*)verifyUserOfYST:(NSArray*)users;
+-(NSArray*)verifyUserOfYST:(NSArray*)users withError:(NSError*)error;
 
 @end
--- YouSet/YSTConnection.m
+++ YouSet/YSTConnection.m
@@ -37,14 +37,85 @@
     return connection;
 }
 
--(void)updateTodo:(YSTToDo *)todo{
-
-    NSLog(@"fazer ystconnection updatetodo");
-    NSLog(@"%@",todo);
+// update todo, envia o todo para o server modificando-o ou criando
+-(void)updateTodo:(YSTToDo*)todo { // assyncrono
+    NSLog(@"[YSTConnection todo] = %@", todo);
 }
 
-- (void)getAllToDosOfUser:(YSTUser *)user {
-    NSLog(@"Pega todos os todos do usuario: %@",user);
+// login do usuario, cria o usuario, ou retorna os todos do usuario caso o usuario exista
+-(NSDictionary*)login:(YSTUser*)user withError:(NSError*)error {
+    return nil;
+    // definir url da area de login
+    NSURL *url = [[NSURL alloc]initWithString:@"http://www."];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url];
+    
+    ////// variaveis em post
+    // setar post string
+    NSString *post = [NSString stringWithFormat:@"&code=%@", [self codeOfServer]];
+    
+    NSData *dataFromConnection = [self makePostRequest:request post: post withError:error];
+    
+    
+    if (!error && dataFromConnection) {
+        NSDictionary *a = [NSJSONSerialization JSONObjectWithData:dataFromConnection options:0 error:&error];
+        if (!error) {
+            return a;
+        }
+    }
+    
+    return nil;
+}
+
+// vai fazer uma requisicao dos todos do usuario passado
+-(NSArray*)getTodosFromUser:(YSTUser*)user withError:(NSError*)error {
+    return nil;
+}
+
+// o usuario do yst vai seguir ou deixar de seguir certo usuario
+-(void)userDevice:(YSTUser*)mainUser willFollow:(BOOL)follow user:(YSTUser*)user { // assyncrono
+    NSLog(@"[YSTConnection userDevice:willFollow:user");
+}
+
+// pegar os seguidores de um user dado
+-(NSArray*)getFollowersFromUser:(YSTUser*)user withError:(NSError*)error {
+    return nil;
+}
+
+// pegar seguidores do usuario do dispositivo
+-(NSArray*)getFollowersFromDeviseUserWithError:(NSError*)error{
+    return nil;
+}
+
+// envia a lista de contatos do usuario do dispositivo e retorna os usuarios que sao do yst
+-(NSArray*)verifyUserOfYST:(NSArray*)users withError:(NSError*)error{
+    return nil;
+}
+
+-(NSData*)makePostRequest:(NSMutableURLRequest*)request post:(NSString*)post withError:(NSError*)error{
+    [request setTimeoutInterval:20];
+    
+    // Encode the post string using NSASCIIStringEncoding and also the post string you need to send in NSData format.
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    // You need to send the actual length of your data. Calculate the length of the post string.
+    NSString *postLength = [NSString stringWithFormat:@"%lu",(unsigned long)[postData length]];
+    
+    // set http method
+    [request setHTTPMethod:@"POST"];
+    // Set HTTP header field with length of the post data.
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    // Also set the Encoded value for HTTP header Field.
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
+    // Set the HTTPBody of the urlrequest with postData.
+    [request setHTTPBody:postData];
+    
+    NSURLResponse *response;
+    
+    return [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+}
+
+-(NSString*)codeOfServer{
+    return @"TRDFyluihijIt76UILUHgTYDeTSD7adokIUHplOU7tdRT&yojikbG7y9";
 }
 
 @end
