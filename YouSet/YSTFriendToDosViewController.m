02fc0c1ef8a22acd1dc321ad6c99cdc2f7a84403
riheldo <riheldomelo@hotmail.com>
2014-08-28 20:38:04 -0300

    fix geruser reload

--- YouSet/YSTFriendToDosViewController.m
+++ YouSet/YSTFriendToDosViewController.m
@@ -368,7 +368,7 @@
 -(void) getUserToDos
 {
     _arrayToDos = [[YSTConnection sharedConnection]getTodosFromUser:self.user withError:nil];
-    NSLog(@"pegou todos");
+    [self reloadArraysOfTodos];
 }
 
 @end
