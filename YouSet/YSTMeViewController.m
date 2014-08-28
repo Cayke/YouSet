be8583c3b883917c6a3a03aef53e570b09b720d7
riheldo <riheldomelo@hotmail.com>
2014-08-28 16:49:18 -0300

    me, organizar células em feitas, em progresso e não realizadas

--- YouSet/YSTMeViewController.m
+++ YouSet/YSTMeViewController.m
@@ -42,6 +42,8 @@
     
     self.meTableView.delegate = self;
     self.meTableView.dataSource = self;
+    [self reloadArraysOfTodos];
+    
     
     self.title = @"Eu";
     
@@ -54,6 +56,7 @@
 }
 - (void)viewWillAppear:(BOOL)animated {
     _toDoMeArray = [YSTToDoStore sharedToDoStore].toDos;
+    [self reloadArraysOfTodos];
     [_meTableView reloadData];
 }
 - (void)didReceiveMemoryWarning
@@ -74,21 +77,58 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
+    
     // Return the number of sections.
+    NSUInteger completed = [_arrayOfCompletedTodos count];
+    NSUInteger incompleted = [_arrayOfIncompleteTodos count];
+    NSUInteger inProgress = [_arrayOfInProgressTodos count];
+    
+    if ( completed > 0 && incompleted > 0 && inProgress > 0) {
+        return 3;
+    } else if ( (completed>0 && incompleted>0) || (completed>0 && inProgress>0) || (incompleted>0 && inProgress>0) ) {
+        return 2;
+    }
+    
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [_toDoMeArray count];
+    if (section == 0) {
+        // todos incompletos
+        return [_arrayOfIncompleteTodos count];
+        
+    } else if (section == 1) {
+        // todos em progresso
+        return [_arrayOfInProgressTodos count];
+        
+    } else if (section == 2) {
+        // todos que estao completos
+        return [_arrayOfCompletedTodos count];
+    }
+    return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     NSString static *cellIdentifier = @"YSTMeTableViewCell";
+    
     YSTMeTableViewCell  *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    YSTToDo *thisToDo = [_toDoMeArray objectAtIndex:indexPath.row];
+    YSTToDo *thisToDo;
+    
+    
+    if (indexPath.section == 0) {
+        thisToDo = [_arrayOfIncompleteTodos objectAtIndex:indexPath.row];
+    } else if (indexPath.section == 1) {
+        thisToDo = [_arrayOfInProgressTodos objectAtIndex:indexPath.row];
+    } else {
+        thisToDo = [_arrayOfCompletedTodos objectAtIndex:indexPath.row];
+    }
+    
+    
     [cell setCellWithTodo:thisToDo];
-   if ([thisToDo getAssigneeOfUser:[YSTUser sharedUser]].status == 0) {
+   
+    if ([thisToDo getAssigneeOfUser:[YSTUser sharedUser]].status == 0) {
        NSString *image = @"none.png";
        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 25.0, 25.0)];
        imageView.image = [UIImage imageNamed:image];
@@ -105,21 +145,78 @@
         imageView.image = [UIImage imageNamed:image];
         [cell setAccessoryView:imageView];
     }
+    
     return cell;
 
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    switch (section) {
+        case 0:
+            return @"Incompletas";
+            break;
+        case 1:
+            return @"Em Progresso";
+            break;
+        case 2:
+            return @"Terminadas";
+            break;
+    }
+    return @"";
 }
 
 #pragma mark - Table view delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
-    [tableView deselectRowAtIndexPath:indexPath animated:NO];
-    YSTToDo *tappedToDO = [_toDoMeArray objectAtIndex:indexPath.row];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    YSTToDo *tappedToDO;
+    
+    if (indexPath.section == 0) {
+        tappedToDO = [_arrayOfIncompleteTodos objectAtIndex:indexPath.row];
+    } else if (indexPath.section == 1) {
+        tappedToDO = [_arrayOfInProgressTodos objectAtIndex:indexPath.row];
+    } else {
+        tappedToDO = [_arrayOfCompletedTodos objectAtIndex:indexPath.row];
+    }
+    
+    
     [tappedToDO incrementStatusOfUser:[YSTUser sharedUser]];
     
     [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+    
+    [self reloadArraysOfTodos];
     [self.meTableView reloadData];
+}
 
+-(void)reloadArraysOfTodos{
+    
+    NSMutableArray *completedTodos, *incompletedTodos, *inProgressTodos;
+    
+    completedTodos = [[NSMutableArray alloc]init];
+    incompletedTodos = [[NSMutableArray alloc]init];
+    inProgressTodos = [[NSMutableArray alloc]init];
+    
+    for (YSTToDo *todo in _toDoMeArray) {
+        int status = [todo getAssigneeOfUser:[YSTUser sharedUser]].status;
+        switch (status) {
+            case 0:
+                [incompletedTodos addObject:todo];
+                break;
+            case 1:
+                [inProgressTodos addObject:todo];
+                break;
+            case 2:
+                [completedTodos addObject:todo];
+                break;
+        }
+    }
+    
+    _arrayOfCompletedTodos = completedTodos;
+    _arrayOfIncompleteTodos = incompletedTodos;
+    _arrayOfInProgressTodos = inProgressTodos;
+    
 }
 
 //- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
