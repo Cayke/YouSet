eef001ece97272ee099914acab0d8dce1d326f90
riheldo <riheldomelo@hotmail.com>
2014-09-10 14:57:57 -0300

    tableviewcell fixed

--- YouSet/YSTMeTableViewCell.m
+++ YouSet/YSTMeTableViewCell.m
@@ -31,34 +31,73 @@
 }
 
 -(void)setCellWithTodo:(YSTToDo *)todo andUserRepresentation:(YSTUser*)userRep{
+    self.description.text = todo.todo;
     _userRepresentation = userRep;
     _todo = todo;
     
-    self.description.text = todo.todo;
-    
-    
-    NSString *s = @"";
-    BOOL todoOfAnotherUser = NO;
-    for (YSTAssignee *a in todo.assignee) {
-        if (a.nameOfUser) {
-            s = [s stringByAppendingFormat:@"%@. ",a.nameOfUser];
-            todoOfAnotherUser = YES;
+    if (_userRepresentation.ID == [YSTUser sharedUser].ID) {
+        // usuario esta olhando seus proprios todos
+        if (todo.idCreatedBy == userRep.ID) {
+            // todo creado pelo usuario, verificar se ele criou o todo para alguem, se nao, esconder rodape
+            
+            NSString *s = @"";
+            BOOL todoToAnotherUser = NO;
+            for (YSTAssignee *a in todo.assignee) {
+                if (a.idUser != userRep.ID) {
+                    s = [s stringByAppendingFormat:@"%@. ",a.nameOfUser];
+                    todoToAnotherUser = YES;
+                }
+            }
+            
+            if (todoToAnotherUser) {
+                // todo feito para outro usuario
+                _usersOfTask.hidden = NO;
+                _usersOfTask.text = [@"To " stringByAppendingString:s];
+                
+            } else {
+                // esconder label
+                _usersOfTask.hidden = YES;
+            }
+            
+        } else {
+            // todo nao foi criado pelo usuario, veio de outra pessoa
+            _usersOfTask.hidden = NO;
+            _usersOfTask.text = [NSString stringWithFormat:@"From %@",todo.createdByName];
         }
-    }
-    
-    _usersOfTask.hidden = !todoOfAnotherUser;
-    _usersOfTask.text = [NSString stringWithFormat:@"To %@",s];
-    
-    if (todo.idCreatedBy != [YSTUser sharedUser].ID) {
-        if (![todo.createdByName isEqualToString:@""]) {
+        
+    } else {
+        // usuario esta olhando todos de seus amigos
+        
+        // todos criados pelo usuario que estamos olhando, nao mostrar
+        if (userRep.ID == todo.idCreatedBy) {
+            // verificar se os todos foram criados para outro usuario
+            
+            NSString *s = @"";
+            BOOL todoToAnotherUser = NO;
+            for (YSTAssignee *a in todo.assignee) {
+                if (a.idUser != userRep.ID) {
+                    s = [s stringByAppendingFormat:@"%@. ",a.nameOfUser];
+                    todoToAnotherUser = YES;
+                }
+            }
+            
+            if (todoToAnotherUser) {
+                // todo feito para outro usuario
+                _usersOfTask.hidden = NO;
+                _usersOfTask.text = [@"To " stringByAppendingString:s];
+                
+            } else {
+                // esconder label
+                _usersOfTask.hidden = YES;
+            }
+            
+        } else {
+            // mostrar quando for criado por outros usuarios, e quando o usuario cria pelo o usuario
             _usersOfTask.hidden = NO;
             _usersOfTask.text = [NSString stringWithFormat:@"From %@",todo.createdByName];
         }
     }
     
-    if (todo.idCreatedBy == userRep.ID && !todoOfAnotherUser) {
-        _usersOfTask.hidden = YES;
-    }
 }
 
 @end
